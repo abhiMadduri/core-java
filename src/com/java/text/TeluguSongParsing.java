6b563cc079ee1d6eaf580ea2aa4a9018e94c029b
Abhishek Madduri <abhishek.madduri@halliburton.com>
2017-03-07 14:04:21 -0600

    writing to end of the file

--- src/com/java/text/TeluguSongParsing.java
+++ src/com/java/text/TeluguSongParsing.java
@@ -1,8 +1,14 @@
 package com.java.text;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -25,15 +31,18 @@ public class TeluguSongParsing {
         String path = "C:\\Tools\\code\\ionic\\praise-app-ionic\\src\\assets\\songs\\songs_telugu.json";
         JSONParser parser = new JSONParser();
         long id = 0;
-
+        RandomAccessFile f = null;
         try {     
-            JSONArray array = (JSONArray) parser.parse(new FileReader(path));
+             f = new RandomAccessFile(new File(path), "rw");
+            //BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+             /*String readUTF = f.readLine();
+             JSONArray array = (JSONArray) parser.parse(new FileReader(path));
+
+            //JSONArray array = (JSONArray) parser.parse(readUTF);
             for (Object o : array) {
                 JSONObject obj = (JSONObject) o;
-                if (obj.get("id") != null) {
-                    id = (long) obj.get("id");
-                }
-            }
+                id = (long) obj.get("id");
+            }*/
         }catch(Exception e) {
             e.printStackTrace();
         }
@@ -59,7 +68,7 @@ public class TeluguSongParsing {
         
         if (!engSongBuilder.toString().isEmpty()) {
             map.put("id", ++id);
-            map.put("title", "Anni Naamamula Kanna");
+            map.put("title", "O Sadbhaktulaaraa");
             map.put("lyrics_en", engSongBuilder.toString());
             if (!telSongBuilder.toString().isEmpty()) {
                 map.put("lyrics_tel", telSongBuilder.toString());
@@ -67,7 +76,17 @@ public class TeluguSongParsing {
             map.put("video_links", "");
         }
         
-        try (FileWriter file = new FileWriter(path, true)) {
+        try {
+            long position = f.length()-1;
+            f.seek(position);
+            //f.writeBytes(StandardCharsets.UTF_8.encode("," + JSONObject.toJSONString(map) +"]").toString());
+            f.writeUTF("," + JSONObject.toJSONString(map) +"]");
+            f.close();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+       /* try (FileWriter file = new FileWriter(path, true)) {
             if (map != null) {
                 file.write("," + JSONObject.toJSONString(map));
                 file.flush();
@@ -76,7 +95,7 @@ public class TeluguSongParsing {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
         //System.out.println(obj.toJSONString());
 
     }
